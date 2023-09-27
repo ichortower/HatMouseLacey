4c2b4136a3f53153d15d7a39b6259ba924c6c591
ichortower <sudo.ichor@proton.me>
2023-09-27 14:25:18 -0400

    Remove unused code from (deprecated) SaveData.

--- SMAPI/SaveData.cs
+++ SMAPI/SaveData.cs
@@ -11,79 +11,14 @@ using System.Collections.Generic;
  * data from old saves. It will be removed in a future release, when that
  * conversion is no longer supported.
  */
-
 namespace ichortower_HatMouseLacey
 {
     internal sealed class LCHatsShown
     {
         public List<int> ids = new List<int>();
     }
-
     internal sealed class LCCrueltyScore
     {
         public int val = 0;
     }
-
-    internal class LCSaveData
-    {
-        /* Tracks which hats you have shown to Lacey. */
-        private static LCHatsShown _hatsShown = null!;
-        /* How many mean points you have earned in heart events. */
-        private static LCCrueltyScore _crueltyScore = null!;
-
-        public static IModHelper HELPER = ModEntry.HELPER;
-
-        public static void ClearCache()
-        {
-            _hatsShown = null;
-            _crueltyScore = null;
-        }
-
-        public static void AddShownHat(int id)
-        {
-            if (_hatsShown is null) {
-                _hatsShown = HELPER.Data.ReadSaveData<LCHatsShown>("HatsShown");
-                if (_hatsShown is null) {
-                    _hatsShown = new();
-                }
-            }
-            _hatsShown.ids.Add(id);
-            HELPER.Data.WriteSaveData<LCHatsShown>("HatsShown", _hatsShown);
-        }
-
-        public static bool HasShownHat(int id)
-        {
-            if (_hatsShown is null) {
-                _hatsShown = HELPER.Data.ReadSaveData<LCHatsShown>("HatsShown");
-                if (_hatsShown is null) {
-                    _hatsShown = new();
-                }
-            }
-            return _hatsShown.ids.Contains(id);
-        }
-
-        public static int CrueltyScore
-        {
-            get {
-                if (_crueltyScore is null) {
-                    _crueltyScore = HELPER.Data.ReadSaveData<LCCrueltyScore>("CrueltyScore");
-                    if (_crueltyScore is null) {
-                        _crueltyScore = new();
-                    }
-                }
-                return _crueltyScore.val;
-            }
-            set {
-                if (_crueltyScore is null) {
-                    _crueltyScore = HELPER.Data.ReadSaveData<LCCrueltyScore>("CrueltyScore");
-                    if (_crueltyScore is null) {
-                        _crueltyScore = new();
-                    }
-                }
-                _crueltyScore.val = value;
-                HELPER.Data.WriteSaveData<LCCrueltyScore>("CrueltyScore", _crueltyScore);
-            }
-        }
-    }
-
 }
